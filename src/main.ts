07d9fc682b551f1457de094bcc571f508f86f9d0
Masami_Nakaoka <neti0326@gmail.com>
2019-07-04 17:03:40 +0000

    出力方法を考え中

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ function doPost(e): GoogleAppsScript.Content.TextOutput {
     const properties: any = PropertiesService.getScriptProperties().getProperties();
     const verificationToken: string = properties.verificationToken;
     const postedToken: string = e.parameter.token;
-    
+
     if (verificationToken !== postedToken) {
         throw new Error('Invalid token');
     }
@@ -23,29 +23,24 @@ function doPost(e): GoogleAppsScript.Content.TextOutput {
             },
             'muteHttpExceptions': true,
         };
-        
+
 
         res = UrlFetchApp.fetch(todoistTaskEndPoint, option);
     }
 
     let result: any;
     if (res.getResponseCode() === 200) {
-        const tasks: string[] = [];
+        let tasksString: string = 'id\tTask Name\tDue Date\n';
         const resContents: any = JSON.parse(res.getBlob().getDataAsString());
         resContents.forEach(task => {
-            tasks.push(task.id);
+            tasksString = tasksString + `${task.id}\t${task.content}\t${task.due.date}\n`;
         });
-        result = {
-            result: 'success',
-            data: tasks,
-        };
+        result = tasksString;
     } else {
-        result = {
-            result: 'failer',
-        };
+        result = 'failer';
     }
 
-    const resultMessage: string = JSON.stringify(result);
-    return ContentService.createTextOutput(resultMessage).setMimeType(ContentService.MimeType.JSON);
+    // const resultMessage: string = JSON.stringify(result);
+    return ContentService.createTextOutput(result);
 
 }
